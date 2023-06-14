79955772117906be6946b7e279631364d0e4bbe8
Kulpot <kulpotwahu@gmail.com>
2023-06-14 20:44:13 +0800

    Add project files.

---
+++ Delegate Chains and Exceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MainClass
+{
+    static void Main()
+    {
+        //Action del = new Action(Moo) + BeNaughty + Goo;
+        Action del = (Action)Moo + BeNaughty + Goo;
+        foreach(Action a in del.GetInvocationList())
+        {
+            try
+            {
+                a();
+            }
+            catch
+            {
+
+            }
+        }
+        
+    }
+    static void Moo()
+    {
+        Console.WriteLine("Moo()");
+    }
+    static void BeNaughty()
+    {
+        throw new Exception();
+    }
+    static void Goo()
+    {
+        Console.WriteLine("Goo()");
+    }
+}
